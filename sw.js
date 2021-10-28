ed30849cef29e80120097ec1076171ed467a6f21
CodesofVysakh <onvysakh309@gmail.com>
2021-10-28 10:30:57 +0530

    Got the installation button

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", function (event) {
+    console.log("Installed" , event);
+});
+self.addEventListener("activate", function (event) {
+    console.log("Activated" , event);
+    return self.clients.claim()
+});
+self.addEventListener("fetch", function (event) {
+    console.log("Service Worker Fetch" , event);
+    event.respondWith(fetch(event.request));
+});
